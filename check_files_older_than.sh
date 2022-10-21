e07e3ca5dee1d7a0717ebdb379ce363ec9d28901
Pablo Adames <pablo.adames@gmail.com>
2022-10-21 00:44:30 -0600

    Coordiated argument passed with find argument
    The files found should have been the ones containing the string thermal in their filename and created with FILE_FREQUENCY_MINUTES from the current time.
    This script should be run every half an interval of FILE_FREQUENCY_MINUTES such that if it does not find at least one file it means none is been created.

--- check_files_older_than.sh
+++ check_files_older_than.sh
@@ -5,9 +5,9 @@
 if (( $# != 1 )); then
     >&2 echo "One argument required for the frequency of archived files in seconds"
 else
-    declare -i FILE_FREQUENCY_SECONDS=$1
+    declare -i FILE_FREQUENCY_MINUTES=$1
     echo "Files to be saved every ${FILE_FREQUENCY_SECONDS} seconds~"  
 fi
 
 # find files that have been modified in the last minute and whose name has thermal in it
-find . -type f -mmin 1 -and -name "*thermal*"  -printf "%m\t%s bytes\t%t\t%u\t%g\t%p\n"
+find . -type f -mmin $FILE_FREQUENCY_MINUTES -and -name "*thermal*"  -printf "%m\t%s bytes\t%t\t%u\t%g\t%p\n"
