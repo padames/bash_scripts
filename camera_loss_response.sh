aa65ba0e7f0baef9bf92a78d4b08907687c44bbf
padames <padames@intelliviewtech.com>
2023-03-21 11:34:34 -0600

    Fixed merge conflicts

--- camera_loss_response.sh
+++ camera_loss_response.sh
@@ -17,16 +17,6 @@ function is_SmrtHVRService_up () {
 }
 
 function infer_camera_loss () {
-    THRESHOLD_IN_SECONDS=$1
-    
-    echo "$(date): Threshold set to $THRESHOLD_IN_SECONDS seconds"
-
-<<<<<<< Updated upstream
-=======
-<<<<<<< Updated upstream
-    function infer_camera_loss () {
-=======
->>>>>>> Stashed changes
     i=0
     while read -a RECORD ; do
         a[i]=${RECORD[0]} # the camera_id
@@ -34,12 +24,7 @@ function infer_camera_loss () {
         camera_name[i]=${RECORD[2]} 
         # echo "${camera_name[i]}"
         i=$((i + 1))
-<<<<<<< Updated upstream
-    done < <(sudo -i  -u intelliview psql ivtdb -t -c "WITH q2 AS ( WITH q1 AS ( SELECT camera_id, camera_name, now() - start_at FROM ivthvr.archive_video_recording JOIN ivthvr.camera_view USING (camera_id) WHERE now() - start_at > '$THRESHOLD_IN_SECONDS seconds'::interval ) INSERT INTO event_log(severity,subsystem,camera_id,message) select 'fatal','Monitor',camera_id,'Camera '|| camera_name || ' failed, rebooting now' FROM q1 RETURNING camera_id ) SELECT camera_id, camera_name FROM q2 JOIN camera_view USING (camera_id);" )
-=======
     done < <(sudo -i  -u intelliview psql ivtdb -t -c "WITH q2 AS ( WITH q1 AS ( SELECT camera_id, camera_name, now() - start_at FROM ivthvr.archive_video_recording JOIN ivthvr.camera_view USING (camera_id) WHERE now() - start_at > '$THRESHOLD_IN_SECONDS seconds'::interval ) INSERT INTO event_log(severity,subsystem,camera_id,message) select 'fatal','Monitor',camera_id,'Camera '|| camera_name || ' (id=' || camera_id || ') failed, rebooting now' FROM q1 RETURNING camera_id ) SELECT camera_id, camera_name FROM q2 JOIN camera_view USING (camera_id);" )
->>>>>>> Stashed changes
->>>>>>> Stashed changes
     
     if (( i < 2 )); then  # the counter exits as 1 if no record was found
         echo "$(date): All good, video files have been created within the last $THRESHOLD_IN_SECONDS seconds from both cameras"
